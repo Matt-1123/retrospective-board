d095cb401a6a6f49ea9acf811c30e8d1840e5af9
Matt Russo <matthew.russo91@gmail.com>
2019-04-08 17:17:34 -0400

    Updates

---
+++ precache-manifest.507efaf14f98ba5057b35b585bc9e6cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc379c3e1f08bb78a0dd",
+    "url": "/retro-board/static/js/runtime~main.d3baa152.js"
+  },
+  {
+    "revision": "ad837337a8ebe58705e9",
+    "url": "/retro-board/static/js/main.2ecfe803.chunk.js"
+  },
+  {
+    "revision": "9f6f345b22f6c508ed1d",
+    "url": "/retro-board/static/js/2.eded8c52.chunk.js"
+  },
+  {
+    "revision": "ad837337a8ebe58705e9",
+    "url": "/retro-board/static/css/main.21bc0805.chunk.css"
+  },
+  {
+    "revision": "ff2514eba5383a8492124efd5692f43f",
+    "url": "/retro-board/index.html"
+  }
+];
